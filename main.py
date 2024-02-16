4b2b412596645f694f4cbe3da9f83671aea3f4ea
PRINCE OP <84593659+shaizansiddiqui1996jbp@users.noreply.github.com>
2024-02-17 02:13:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import io
 import os
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!",intents=intents)
 
 @bot.command()
 async def ping(ctx):
